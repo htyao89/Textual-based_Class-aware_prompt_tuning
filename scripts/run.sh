e74b3ead791166efa998e6f73342aa88b731a959
htyao89 <74002548+coldrainyht@users.noreply.github.com>
2023-11-20 12:55:51 +0800

    Create run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh eurosat 1.0  4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh oxford_pets 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh food101 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh oxford_flowers 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh stanford_cars 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh ucf101 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh dtd 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh fgvc_aircraft 1.0 4
+CUDA_VISIBLE_DEVICES=0 bash base2new_train.sh caltech101 1.0 4
